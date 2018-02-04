85284d17c40c3d55dc6b758fb07e7e5e9a3ab5b8
tarasromil <tarasromil@gmail.com>
2018-02-04 23:11:23 +0200

    fixed import

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 export { default as withInputs } from './with-inputs';
 
 export { default as withToggle } from './with-toggle';
+
+export { default as setRoles } from './set-roles';
+
+export { default as withRoles } from './with-roles';
